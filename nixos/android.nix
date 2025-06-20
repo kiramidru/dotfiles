d2a4108a2627701d45d345895b9e5c1dca2d9ab3
Kira Midru <kirubelmidru@gmail.com>
2025-06-20 12:28:48 +0300

    fix android

--- nixos/android.nix
+++ nixos/android.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.android-studio];
   programs.adb.enable = true;
-  users.users.kira.extraGroups = ["kvm", "abdusers"];
+  users.users.kira.extraGroups = ["kvm" "abdusers"];
 }
