870b538c4e6e9ee0b232388431dd24f49e4f7bdd
Kira Midru <kirubelmidru@gmail.com>
2025-09-06 02:45:39 +0300

    add: dnsmasq folder

--- nixos/spotify.nix
+++ nixos/spotify.nix
@@ -8,13 +8,9 @@
     enable = true;
     settings = {
       "listen-address" = "127.0.0.1";
-      "conf-dir" = "/etc/dnsmasq";
-      "resolv-file" = "/etc/dnsmasq-resolv.conf";
+      "conf-dir" = "/etc/dnsmasq/dnsmasq-conf.conf";
+      "resolv-file" = "/etc/dnsmasq/dnsmasq-resolv.conf";
     };
   };
   environment.etc."dnsmasq".source = ./dnsmasq;
-  environment.etc."dnsmasq-resolv.conf".text = ''
-    nameserver 1.1.1.1
-    nameserver 1.0.0.1
-  '';
 }
