51318e82b640330dcdd7e9d73bbbb55e4cd5b769
Kira Midru <kirubelmidru@gmail.com>
2025-09-06 01:48:57 +0300

    add: dnsmasq

--- nixos/spotify.nix
+++ nixos/spotify.nix
@@ -1,38 +1,16 @@
 { pkgs, ... }:
 {
   users.users.kira = {
-    packages = with pkgs; [
-      spotify
-    ];
+    packages = with pkgs; [ spotify ];
   };
+
   services.dnsmasq = {
     enable = true;
-    extraConfig = ''
-      conf-file=/etc/dnsmasq.d/spotify-blocklist.conf
-    '';
-  };
-
-  systemd.services."spotify-blocklist-update" = {
-    description = "Fetch Spotify ad domains and reload dnsmasq";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = ''
-        curl -s https://raw.githubusercontent.com/x0uid/SpotifyAdBlock/master/SpotifyBlocklist.txt \
-          | tr ' ' '\n' \
-          | sed 's/^/address=\/&\/0.0.0.0/' \
-          > /etc/dnsmasq.d/spotify-blocklist.conf
-        systemctl reload dnsmasq.service
-      '';
+    settings = {
+      "listen-address" = "127.0.0.1";
+      "conf-dir" = "/etc/dnsmasq";
     };
   };
+  environment.etc."dnsmasq".source = ./dnsmasq;
 
-  systemd.timers."spotify-blocklist-update" = {
-    description = "Timer to update Spotify ad blocklist daily";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-  };
 }
--- nixos/user.nix
+++ nixos/user.nix
@@ -20,7 +20,6 @@
       obsidian
       postman
       qbittorrent
-      spotify
       telegram-desktop
       todoist-electron
       unityhub
