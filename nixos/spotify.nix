0fb15e8cf5cfc61624f0ac2695e70e2dea92cf38
Kira Midru <kirubelmidru@gmail.com>
2025-09-06 03:33:03 +0300

    update: blocked address

--- nixos/spotify.nix
+++ nixos/spotify.nix
@@ -8,9 +8,10 @@
     enable = true;
     settings = {
       "listen-address" = "127.0.0.1";
-      "conf-dir" = "/etc/dnsmasq/dnsmasq-conf.conf";
+      "conf-file" = "/etc/dnsmasq/dnsmasq-conf.conf";
       "resolv-file" = "/etc/dnsmasq/dnsmasq-resolv.conf";
     };
   };
+
   environment.etc."dnsmasq".source = ./dnsmasq;
 }
